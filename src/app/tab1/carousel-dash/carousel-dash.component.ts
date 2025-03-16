6d6de7e7fb36d47024e95fb6a150c283c73c811d
guti10x <gt104515@gmail.com>
2025-03-16 21:25:39 +0100

    Parametrizar título y tipo de carrusel

--- src/app/tab1/carousel-dash/carousel-dash.component.ts
+++ src/app/tab1/carousel-dash/carousel-dash.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -15,20 +15,26 @@ export class CarouselDashComponent  implements OnInit {
     { title: '💡 Recomendación', content: 'Prueba reducir el tiempo en redes sociales antes de dormir.' }
   ];
 
+  @Input() tipoCarrusel!: string;
+
+  // Índice del slide actual del carrusel
   currentIndex = 0;
+  
 
   constructor() {}
 
   ngOnInit() {
     setInterval(() => {
       this.nextSlide();
-    }, 5000); // Cambia cada 5 segundos
+    }, 3000); // Cambia cada 3 segundos
   }
 
+  // Avanza al siguiente slide del carrusel
   nextSlide() {
     this.currentIndex = (this.currentIndex + 1) % this.items.length;
   }
 
+  // Retrocede al slide anterior del carrusel
   prevSlide() {
     this.currentIndex = (this.currentIndex - 1 + this.items.length) % this.items.length;
   }
