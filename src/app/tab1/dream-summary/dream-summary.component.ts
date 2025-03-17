364a3ecc3fd1c14dd230f20cb284722c3c0d6eb9
guti10x <gt104515@gmail.com>
2025-03-17 20:15:49 +0100

    Update estilos daswhare de datos de sueño

--- src/app/tab1/dream-summary/dream-summary.component.ts
+++ src/app/tab1/dream-summary/dream-summary.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dream-summary',
   templateUrl: './dream-summary.component.html',
   styleUrls: ['./dream-summary.component.scss'],
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
+
 export class DreamSummaryComponent  implements OnInit {
 
   constructor() { }
@@ -19,15 +21,36 @@ export class DreamSummaryComponent  implements OnInit {
     this.loadSleepData();
   }
 
-  
   loadSleepData() {
-    // Datos estáticos para demostración, puedes modificar esto
     this.hoursOfSleep = 8;
-    this.sleepQuality = 'Good';  // Puedes modificar el valor según tus necesidades
-    this.timeSlept = '10:30 PM - 6:30 AM';  // Tiempo en el que se durmió
+    this.sleepQuality = 'Good';  
+    this.timeSlept = '10:30 PM - 6:30 AM';
   }
 
-
+  getSleepQualityClass(quality: string): string {
+    switch (quality.toLowerCase()) {
+      case 'good':
+        return 'good-sleep';
+      case 'medium':
+        return 'medium-sleep';
+      case 'bad':
+        return 'bad-sleep';
+      default:
+        return 'medium-sleep'; // Por defecto
+    }
+  }
   
+  getSleepQualityIcon(quality: string): string {
+    switch (quality.toLowerCase()) {
+      case 'good':
+        return 'happy';
+      case 'medium':
+        return 'alert';
+      case 'bad':
+        return 'sad';
+      default:
+        return 'help-circle';
+    }
+  }
 
 }
